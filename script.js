90924c1398261b26bf711895ecc6757f46c79590
dina <naveenkumar.coder@gmail.com>
2023-03-29 11:46:53 +0530

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Search Bar */
+
+document.querySelector("#searchInput").addEventListener("keyup", function (e) {
+  // UI Element
+  let namesLI = document.getElementsByClassName("name");
+
+  // Get Search Query
+  let searchQuery = searchInput.value.toLowerCase();
+
+  // Search Compare & Display
+  for (let index = 0; index < namesLI.length; index++) {
+    const name = namesLI[index].textContent.toLowerCase();
+
+    if (name.includes(searchQuery)) {
+      namesLI[index].style.display = "block";
+    } else {
+      namesLI[index].style.display = "none";
+    }
+  }
+});
